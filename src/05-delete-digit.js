16725dbefc6b0f7e0d16638bb1c64a114a316e6b
Denis <dean.angell@mail.ru>
2021-04-08 14:47:19 +0300

    feat: add solution 05

--- src/05-delete-digit.js
+++ src/05-delete-digit.js
@@ -9,8 +9,26 @@
  * For n = 152, the output should be 52
  *
  */
-function deleteDigit(/* n */) {
-  throw new Error('Not implemented');
+function deleteDigit(num) {
+  const digit = [];
+  let n = num;
+  while (n > 0) {
+    digit.unshift(n % 10);
+    n = Math.floor(n / 10);
+  }
+  let remIndx = -1;
+  let flag = true;
+  for (let i = 0; i < digit.length - 1; i++) {
+    if (digit[i] < digit[i + 1] && flag) {
+      remIndx = i;
+      flag = false;
+    }
+  }
+  if (flag) {
+    remIndx = digit.length - 1;
+  }
+  digit.splice(remIndx, 1);
+  const result = parseInt(digit.join(''), 10);
+  return Number.isNaN(result) ? 0 : result;
 }
-
 module.exports = deleteDigit;
