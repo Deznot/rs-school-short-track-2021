89fd208a5582bda79a9dcb3f5191d8ce5d13d796
Denis <dean.angell@mail.ru>
2021-04-09 20:01:28 +0300

    feat: add solution 10 binary search

--- src/10-find-index.js
+++ src/10-find-index.js
@@ -11,8 +11,22 @@
  * For ([1, 2, 3], 2) should return 1
  *
  */
-function findIndex(/* array, value */) {
-  throw new Error('Not implemented');
+function findIndex(array, value) {
+  let low = 0;
+  let high = array.length - 1;
+  let res;
+  while (low <= high) {
+    const mid = Math.round((low + high) / 2);
+    const guess = array[mid];
+    if (guess === value) {
+      res = mid;
+      break;
+    } else if (guess > value) {
+      high = mid - 1;
+    } else {
+      low = mid + 1;
+    }
+  }
+  return res;
 }
-
 module.exports = findIndex;
